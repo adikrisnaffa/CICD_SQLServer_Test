ae0106d0b1310dfb474b60878ddb95b70c96ce02
adikrisnaffa <adikrisnanugraha@gmail.com>
2023-10-26 10:14:07 +0700

    Load the Initial Solution

---
+++ CICD_DB/dbo/Tables/EmpDetails.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[EmpDetails] (
+    [ID]           INT        NOT NULL,
+    [EmpFirstName] NCHAR (10) NULL,
+    [EmpLastName]  NCHAR (10) NULL,
+    [Department]   NCHAR (10) NULL,
+    [Phone]        NCHAR (10) NULL,
+    [Salary]       MONEY      NULL,
+    CONSTRAINT [PK_EmpDetails] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
